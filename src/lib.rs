65b7390d584b264bf46ccaec6a5dee306ba7c586
figsoda <figsoda@pm.me>
2020-10-23 12:22:56 -0400

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 //! # expand
-//! 
+//!
 //! Macro to expand byte string literals
-//! 
-//! 
+//!
+//!
 //! ## Usage
-//! 
+//!
 //! ```rust
 //! use expand::expand;
-//! 
+//!
 //! assert_eq!(
 //!     &expand!([@b"Hello,", b' ', @b"world", b'!']),
 //!     b"Hello, world!"
 //! );
-//! 
+//!
 //! if let expand!([@b"patt", x, y, b'n', ..]) = b"pattern matching" {
 //!     assert_eq!(x, &b'e');
 //!     assert_eq!(y, &b'r');
